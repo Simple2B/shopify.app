8662e1ae6cb880af18aeffaaaf174959c5c2eb2a
Vitaly <dagger180@gmail.com>
2021-02-05 09:47:59 +0000

    begin to integrate flask-app with shopify

---
+++ app/models/token.py
@@ -0,0 +1,13 @@
+from app import db
+from app.models.utils import ModelMixin
+
+
+class User(db.Model, ModelMixin):
+
+    __tablename__ = 'token'
+
+    id = db.Column(db.Integer, primary_key=True)
+    access_token = db.Column(db.String(60), unique=True)
+
+    def __str__(self):
+        return '<Access token: %s>' % self.access_token
