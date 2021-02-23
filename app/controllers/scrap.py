aacc93d1169bf021c735c40577d20d10f9c6a5df
Alik <tsyboom@gmail.com>
2021-02-23 11:50:50 +0200

    fix scrap-img script

--- app/controllers/scrap.py
+++ app/controllers/scrap.py
@@ -1,4 +1,5 @@
 import urllib
+import time
 from urllib.request import Request, urlopen
 from bs4 import BeautifulSoup
 from app.logger import log
@@ -10,19 +11,31 @@ def get_html(item_id: int):
     return html
 
 
-def scrap_img(item_id: int):
+def check_soup(item_id: int):
     try:
         soup = BeautifulSoup(get_html(item_id), 'html.parser')
+        return soup
     except urllib.error.HTTPError:
-        log(log.INFO, 'Invalid response')
         return False
+
+
+def scrap_img(item_id: int):
+    """Get all item pictures from VidaXL
+
+    Args:
+        item_id (int): Item_id in VidaXL API
+
+    Returns:
+        [JSON]: Item_id, images quantity, list of images urls
+    """
+    soup = check_soup(item_id)
+    while not soup:
+        soup = check_soup(item_id)
+        log(log.INFO, 'Invalid Response')
+        time.sleep(.5)
     gallery = soup.find('div', class_='media-gallery')
     img_container = gallery.findAll('a')
-    images = [i.attrs['href'] for i in img_container]
-    if 'missing_image' in images:
-        for img in images:
-            if 'missing_image' in img:
-                images.pop(img)
+    images = [i.attrs['href'] for i in img_container if 'missing_image' not in i]
     return {
         'item_id': item_id,
         'qty': len(images),
