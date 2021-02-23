bda76e01161489f4c244f3170f382f5662e8ea11
Alik <tsyboom@gmail.com>
2021-02-23 13:18:46 +0200

    add MoM price scrapper

---
+++ app/controllers/mom_price_scrapper.py
@@ -0,0 +1,31 @@
+import urllib
+from urllib.request import Request, urlopen
+from bs4 import BeautifulSoup
+from app.logger import log
+
+
+def get_html(item_id: int):
+    req = Request(
+        'https://www.mallofmaster.nl/catalogsearch/result/?q=' +
+        str(item_id), headers={'User-Agent': 'Mozilla/5.0'}
+        )
+    html = urlopen(req)
+    return html
+
+
+def scrap_img(item_id: int):
+    """Get price from Mall of Master NL
+
+    Args:
+        item_id (int): Code item in VidaXL, SKU in Mall of Master
+
+    Returns:
+        [int]: Price in Euro
+    """
+    try:
+        soup = BeautifulSoup(get_html(item_id), 'html.parser')
+    except urllib.error.HTTPError as e:
+        log(log.ERROR, 'Invalid response [%s]', e)
+        return False
+    price = soup.find('span', class_='price')
+    return int(price.string[2:])
