09576fd2bdca993928cd44b42dbd0f5af29e09a2
Vitaly <dagger180@gmail.com>
2021-02-25 10:56:37 +0000

    add retry policy

--- app/controllers/__init__.py
+++ app/controllers/__init__.py
@@ -1,2 +1,4 @@
-from .scrap import scrap_img   # noqa f401
-from .update_product import update_product   # noqa f401
+# flake8: noqa f401
+from .scrap import scrap_img
+from .upload_product import upload_product
+from .retry_request import retry_get_request
--- tests/test_vida_xl.py
+++ tests/test_vida_xl.py
@@ -2,10 +2,14 @@ import pytest
 import requests
 
 from requests.auth import HTTPBasicAuth
-from app.vida_xl import update_products
-from flask import current_app
-
+from app.controllers import upload_product, retry_get_request
 from app import create_app
+
+from config import TestingConfig as conf
+
+
+URL = f"{conf.VIDAXL_API_BASE_URL}/api_customer/products"
+AUTH = HTTPBasicAuth(conf.USER_NAME, conf.API_KEY)
 
 
 @pytest.fixture
@@ -20,15 +24,20 @@ def client():
         app_ctx.pop()
 
 
-def test_update_products(client):
-    res = update_products()
+def _test_upload_products(client):
+    res = upload_product()
     assert res
 
 
 def test_get_products(client):
-    url = f"{current_app.config['VIDAXL_API_BASE_URL']}/api_customer/products"
-    auth = HTTPBasicAuth(current_app.config["USER_NAME"], current_app.config["API_KEY"])
-    response = requests.get(f"{url}?offset=0", auth=auth)
+    response = requests.get(f"{URL}?offset=0", auth=AUTH)
     assert response.status_code == 200
     data = response.json().get('data', '')
     assert data
+
+
+def test_retry_get_request(client):
+    for _ in range(100):
+        response = retry_get_request(URL, auth=AUTH)
+        print(_)
+        assert response
