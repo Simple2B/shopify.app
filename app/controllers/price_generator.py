6afb43093fb9270a4e9d424d1bfa5ce6ff85256d
tsyboom <tsyboom@gmail.com>
2021-03-01 09:48:43 +0000

    work

--- app/controllers/price_generator.py
+++ app/controllers/price_generator.py
@@ -1,3 +1,9 @@
+import requests
+from flask import current_app
+from requests.auth import HTTPBasicAuth
+from app.shopify_api import Product
+
+
 def price_generator(purchase_price, mom=None, margin=None, round_to=None):
     """Generate new price
 
@@ -21,3 +27,34 @@ def price_generator(purchase_price, mom=None, margin=None, round_to=None):
     elif round_to:
         new_price = round(float(purchase_price)) - (1 - float(round_to))
     return {'product': {'price': str(new_price)}}
+
+
+def get_purchase_price(item_id):
+    """Get purchase price from VidaXL
+
+    Args:
+        item_id (int or str):
+
+    Returns:
+        Str: Purchase price
+    """
+    response = requests.get(
+        f"https://b2b.vidaxl.com/api_customer/products?code_eq={item_id}",
+        auth=HTTPBasicAuth(
+            current_app.config["USER_NAME"], current_app.config["API_KEY"]
+        ),
+    )
+    if response.status_code == 200:
+        return response.json()['data'][0]['price']
+    else:
+        return f"Statucs code: {response.status_code}"
+
+
+def set_price(item_id, price, token, url):
+    data = {'product': {'price': str(price)}}
+    new_product = Product(token, url)
+    new_product.update_product(item_id, data)
+    if response.status_code == 200:
+        return f'Price item {item_id} has been changed.'
+    else:
+        return f"Statucs code: {response.status_code}"
