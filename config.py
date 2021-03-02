ba41e18490de69caa1789b240eb78b9c63ace73a
Vitaly <dagger180@gmail.com>
2021-03-02 13:11:56 +0000

    update shop model

--- config.py
+++ config.py
@@ -15,15 +15,12 @@ class BaseConfig(object):
     SHOPIFY_SECRET = os.environ.get("SHOPIFY_SECRET", "")
     APP_NAME = os.environ.get("APP_NAME", "")
     SHOPIFY_DOMAIN = os.environ.get("SHOPIFY_DOMAIN", "")
-    VERSION_API = os.environ.get("VERSION_API", "2021-21")
+    VERSION_API = os.environ.get("VERSION_API", "2021-01")
     WEBHOOK_APP_UNINSTALL_URL = os.environ.get("WEBHOOK_APP_UNINSTALL_URL", "")
     INSTALL_REDIRECT_URL = os.environ.get("INSTALL_REDIRECT_URL", "")
     ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN", "")
     NUMBER_OF_REPETITIONS = os.environ.get("NUMBER_OF_REPETITIONS", "10")
     SLEEP_TIME = os.environ.get("SLEEP_TIME", ".5")
-
-    # Private app
-    X_SHOPIFY_ACCESS_TOKEN = os.environ.get("X_SHOPIFY_ACCESS_TOKEN", "")
 
     # VidaXL credentials
     VIDAXL_API_BASE_URL = os.environ.get(
