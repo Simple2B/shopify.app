d86f962c4eedee3ab07665d7c4bc5aad31bff712
tsyboom <tsyboom@gmail.com>
2021-03-02 15:52:03 +0000

    add price generator

--- app/controllers/mom_price_scrapper.py
+++ app/controllers/mom_price_scrapper.py
@@ -29,3 +29,29 @@ def scrap_price(item_id: int):
         return False
     price = soup.find('span', class_='price')
     return int(price.string[2:])
+
+
+def price_generator(purchase_price, mom=None, margin=0, round_to=None):
+    """Generate new price
+
+    Args:
+        purchase_price (str or int): Purchase price
+        mom (str or int, optional): Price at Mall of Master. Defaults to None.
+        margin (str or int, optional): Margin procent. Defaults to None.
+        round_to (str or int, optional): Round to (e.g. xx.99) Defaults to None.
+
+    Returns:
+        [str]: Price
+    """
+    new_price = None
+    if mom:
+        if float(purchase_price) > float(mom):
+            new_price = float(purchase_price) * (float(margin)/100+1)
+        else:
+            if mom > 1:
+                new_price = str(mom-1)[:-2] + '.' + str(round_to)
+            else:
+                new_price = float(purchase_price) * (float(margin)/100+1)
+    else:
+        new_price = float(purchase_price) * (float(margin)/100+1)
+    return str(new_price)
