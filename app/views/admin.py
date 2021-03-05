6244055a2a7cdfee2fb162e10bc1e21e3a52ef10
Vitaly <dagger180@gmail.com>
2021-03-05 11:48:08 +0000

    add flash message

--- app/views/admin.py
+++ app/views/admin.py
@@ -2,8 +2,6 @@ import io
 from datetime import datetime
 
 from flask import (
-    redirect,
-    url_for,
     render_template,
     Blueprint,
     flash,
@@ -14,6 +12,7 @@ from app.forms import ConfigurationForm
 from app.models import Configuration, Product
 from app.logger import log
 from app.controllers import (
+    shopify_auth_required,
     update_categories,
     update_access_token,
 )
@@ -22,30 +21,33 @@ admin_blueprint = Blueprint("admin", __name__, url_prefix="/admin")
 
 
 @admin_blueprint.route("/<int:shop_id>", methods=["GET", "POST"])
-# @shopify_auth_required
+@shopify_auth_required
 def admin(shop_id):
     form = ConfigurationForm(request.form)
     form.shop_id = shop_id
+    vidaxl_prefix_conf = Configuration.get_value(shop_id, "LEAVE_VIDAXL_PREFIX")
     if form.validate_on_submit():
+        form_vidaxl_prefix = form.leave_vidaxl_prefix.data
         log(log.DEBUG, "Form validate with succeed!")
-        Configuration.set_value(
-            shop_id, "LEAVE_VIDAXL_PREFIX", form.leave_vidaxl_prefix.data
-        )
+        if not Configuration.get_value(shop_id, "LEAVE_VIDAXL_PREFIX") == form_vidaxl_prefix:
+            Configuration.set_value(
+                shop_id, "LEAVE_VIDAXL_PREFIX", form_vidaxl_prefix
+            )
         if "category_rules_file" in request.files:
             update_categories(shop_id, request.files["category_rules_file"])
         if form.private_app_access_token.data:
             update_access_token(shop_id, form.private_app_access_token.data)
-        return redirect(url_for("admin.admin", shop_id=shop_id))
+        flash("Configuration saved", "success")
+        log(log.INFO, "Configuration saved")
+        return render_template("index.html", form=form, **request.args)
     if form.is_submitted():
         log(log.ERROR, "%s", form.errors)
         for error in form.errors:
             for msg in form.errors[error]:
                 flash(msg, "warning")
 
-    form.leave_vidaxl_prefix.data = Configuration.get_value(
-        shop_id, "LEAVE_VIDAXL_PREFIX"
-    )
-    return render_template("index.html", form=form)
+    form.leave_vidaxl_prefix.data = vidaxl_prefix_conf
+    return render_template("index.html", form=form, **request.args)
 
 
 @admin_blueprint.route("/all_categories", methods=["GET"])
