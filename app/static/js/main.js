ea0dcf342c885ffdc617e73140dc38d9ff223220
Vitaly <dagger180@gmail.com>
2021-03-05 19:16:25 +0000

    modify template

--- app/static/js/main.js
+++ app/static/js/main.js
@@ -1,16 +1,2 @@
 // custom javascript
 // test js (need refracting)
-// const drawingLogCloseWrapper = document.getElementById('drawing_log_close_panel_id');
-// const drawingLogClosePanel = document.getElementById('drawing_log_hidden_id');
-// drawingLogCloseWrapper.addEventListener('click', (e) => {
-//   e.preventDefault();
-//   let changeDrawingLogImg = document.querySelector('#drawing_log_close_panel_id img').getAttribute('src');
-//   drawingLogClosePanel.classList.toggle('hidden');
-//   if (changeDrawingLogImg == "/static/images/up_direction_element.svg") {
-//     document.querySelector('#drawing_log_close_panel_id img').setAttribute('src', "/static/images/direction_element_bottom.svg");
-//   } else {
-//     document.querySelector('#drawing_log_close_panel_id img').setAttribute('src', "/static/images/up_direction_element.svg");
-//   };
-// });
-
-
