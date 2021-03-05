ddcbc1dd19a28befc5d5ea068021d6f89cfc9ed0
Vitaly <dagger180@gmail.com>
2021-03-05 09:27:17 +0000

    add images model

--- app/models/image.py
+++ app/models/image.py
@@ -3,21 +3,17 @@ from app.models.utils import ModelMixin
 from sqlalchemy.orm import relationship
 
 
-class ShopProduct(db.Model, ModelMixin):
-    """links to shop products"""
-
-    __tablename__ = "shop_products"
+class Image(db.Model, ModelMixin):
+    __tablename__ = "images"
 
     id = db.Column(db.Integer, primary_key=True)
-    shop_id = db.Column(db.Integer, db.ForeignKey("shops.id"), nullable=False)
-    shop_product_id = db.Column(db.Integer, nullable=False)
     product_id = db.Column(db.Integer, db.ForeignKey("products.id"), nullable=False)
+    url = db.Column(db.String, default="")
 
-    shop = relationship("Shop")
     product = relationship("Product")
 
     def __repr__(self) -> str:
         return self.__str__()
 
     def __str__(self):
-        return f"<{self.product_id}: ({self.shop_product_id})>"
+        return f"<[{self.id}]: {self.url}(product_id: {self.product_id})>"
