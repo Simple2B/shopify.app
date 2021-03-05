e2946237990f4812229f40cde8a1d9161c553016
Vitaly <dagger180@gmail.com>
2021-03-05 10:13:44 +0000

    fix test

--- tests/test_scrappy_img.py
+++ tests/test_scrappy_img.py
@@ -1,10 +1,11 @@
 import pytest
 from datetime import datetime
 
-from app import create_app
+from app import db, create_app
 from app.models import Product
 from app.controllers import scrap
 from app.logger import log
+from .utils import fill_db_by_test_data
 
 
 @pytest.fixture
@@ -15,7 +16,12 @@ def client():
     with app.test_client() as client:
         app_ctx = app.app_context()
         app_ctx.push()
+        db.drop_all()
+        db.create_all()
+        fill_db_by_test_data()
         yield client
+        db.session.remove()
+        db.drop_all()
         app_ctx.pop()
 
 
