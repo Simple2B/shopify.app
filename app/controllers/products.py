65cd40a7510037f79e8d7cd527df77dc9504e602
Vitaly <dagger180@gmail.com>
2021-03-14 20:13:23 +0000

    add upload_csv

--- app/controllers/products.py
+++ app/controllers/products.py
@@ -1,7 +1,20 @@
+import tempfile
+import requests
+import csv
+from io import TextIOWrapper
 from datetime import datetime
 
 import shopify
-from app.models import Configuration, Category, Product, Shop, ShopProduct
+from app import db
+from app.models import (
+    Configuration,
+    Category,
+    Product,
+    Shop,
+    ShopProduct,
+    Image,
+    Description,
+)
 from .price import get_price
 from .scrap import scrap_img, scrap_description
 from app.logger import log
@@ -59,7 +72,109 @@ def upload_product_old(
         log(log.DEBUG, "Product created. Product id: [%d]", product_id)
 
 
+def upload_csv(csv_url, limit=None):
+
+    def update_db_from_file_stream(csv_dict_reader, limit=None):
+        number_db_commit = 0
+        next_product_id = Product.query.count() + 1
+        for i, vidaxl_prod in enumerate(csv_dict_reader):
+            update_date = datetime.now()
+            sku = vidaxl_prod["SKU"]
+            title = vidaxl_prod["Product_title"]
+            price = float(vidaxl_prod["B2B price"])
+            quantity = float(vidaxl_prod["Stock"])
+            category_path = vidaxl_prod["Category"]
+            description = vidaxl_prod["HTML_description"]
+            images = [
+                vidaxl_prod[image]
+                for image in vidaxl_prod
+                if image.startswith("Image") and vidaxl_prod[image] != ""
+            ]
+            log(log.DEBUG, "Get images (%d)", len(images))
+            vidaxl_id = vidaxl_prod["EAN"]
+            prod = Product.query.filter(Product.sku == sku).first()
+            if prod:
+                if title != prod.title:
+                    prod.title = title
+                    prod.is_changed = True
+                if category_path != prod.category_path:
+                    prod.category_path = category_path
+                    prod.is_changed = True
+                if price != prod.price:
+                    prod.price = price
+                    prod.is_changed = True
+                if quantity != prod.qty:
+                    prod.qty = quantity
+                    prod.is_changed = True
+                if description != prod.description[0].text:
+                    prod.description[0].text = description
+                    prod.is_changed = True
+
+                if prod.is_deleted:
+                    prod.is_changed = True
+
+                prod.updated = update_date
+                prod.is_deleted = False
+
+                # update images
+                for i, image in enumerate(images):
+                    if not image:
+                        continue
+                    if len(images) > len(prod.images):
+                        Image(product_id=prod.id, url=image).save(commit=False)
+                        number_db_commit += 1
+                        continue
+                    prod.images[i].url = image
+
+                prod.save(commit=False)
+                number_db_commit += 1
+            else:
+                Product(
+                    vidaxl_id=vidaxl_id,
+                    sku=sku,
+                    title=title,
+                    category_path=category_path,
+                    price=price,
+                    qty=quantity,
+                ).save(commit=False)
+                for image in images:
+                    Image(product_id=next_product_id, url=image).save(commit=False)
+                Description(product_id=next_product_id, text=description).save(commit=False)
+                log(log.DEBUG, "Add new product[%d: %s] to db", next_product_id, title)
+                next_product_id += 1
+                number_db_commit += 1
+            if number_db_commit > 100:
+                db.session.commit()
+            if limit and i + 1 >= limit:
+                db.session.commit()
+                break
+
+    with tempfile.NamedTemporaryFile(mode="w+b") as csv_file:
+        response = requests.get(csv_url, stream=True)
+        if response.status_code != 200:
+            log(log.ERROR, "Invalid response [%s]", response)
+            return None
+        for chunk in response.iter_content(chunk_size=100 * 1024):
+            csv_file.write(chunk)
+        try:
+            with open(csv_file.name, "rb") as f:
+                csv_dict_reader = csv.DictReader(
+                    TextIOWrapper(f, encoding="utf-8"), delimiter=","
+                )
+                update_db_from_file_stream(csv_dict_reader, limit)
+                return True
+        except Exception as exc:
+            log(log.ERROR, "Invalid file; exception: (%s)", exc)
+            return None
+            if not f.read():
+                log(log.WARNING, "CSV File is empty")
+                return None
+
+
 def download_products(limit=None):
+    csv_url = Configuration.get_value(1, "SCV_PATH", path="/")
+    if csv_url:
+        return upload_csv(csv_url)
     vida = VidaXl()
     update_date = datetime.now()
     log(log.INFO, "Start update VidaXl products - %s", "All" if not limit else limit)
@@ -208,9 +323,7 @@ def upload_product(shop_id: int, limit=None):
                             ],
                             images=[
                                 {"src": img}
-                                for img in scrap_img(product).get(
-                                    "images", []
-                                )
+                                for img in scrap_img(product).get("images", [])
                             ],
                         )
                     )
