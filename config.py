a152dcb77362dcd6a44967bf4db5ee015cad0b5f
Nikolay <nchernov@gmail.com>
2021-03-14 14:33:33 +0000

    revert SERVER_NAME

--- config.py
+++ config.py
@@ -21,7 +21,7 @@ class BaseConfig(object):
     ACCESS_TOKEN = os.environ.get("ACCESS_TOKEN", "")
     NUMBER_OF_REPETITIONS = os.environ.get("NUMBER_OF_REPETITIONS", "10")
     SLEEP_TIME = float(os.environ.get("SLEEP_TIME", "1"))
-    SERVER_NAME = os.environ.get("SERVER_NAME")
+    HOST_NAME = os.environ.get("HOST_NAME", "localhost")
     SHOPIFY_PRIVATE_APP_PASSWORD = os.environ.get("SHOPIFY_PRIVATE_APP_PASSWORD", "")
 
     # VidaXL credentials
