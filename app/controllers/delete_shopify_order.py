cbd1de9014fc29abc5e68c0cb1e592dfa2314564
tsyboom <tsyboom@gmail.com>
2021-03-19 10:15:31 +0100

    delete order - add reason,sending mail

--- app/controllers/delete_shopify_order.py
+++ app/controllers/delete_shopify_order.py
@@ -13,11 +13,16 @@ def delete_order(order_id):
         [Bool]: [Shopify response]
     """
     url = f'{conf.SHOPIFY_DOMAIN}/admin/api/2021-01/orders/{order_id}/cancel.json'
+    data = {
+        'reason': 'other',
+        'email': 'true',
+        'note': 'invalid address or product is out of stock'
+    }
     headers = {
         'User-Agent': 'Mozilla/5.0',
         "X-Shopify-Access-Token": conf.SHOPIFY_PRIVATE_APP_PASSWORD
     }
-    response = requests.post(url, headers=headers)
+    response = requests.post(url, headers=headers, json=data)
     if response.status_code == 200:
         log(log.INFO, 'Order with ID [%d] deleted.', order_id)
         return True
